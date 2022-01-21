5ea69291e4545ea78ff71b20ab99856cac944a02
David Lutterkort <lutter@watzmann.net>
2022-01-21 12:13:10 -0800

    store: Rename some of the structs in writable

--- store/postgres/src/subgraph_store.rs
+++ store/postgres/src/subgraph_store.rs
@@ -36,7 +36,7 @@ use crate::{
     primary,
     primary::{DeploymentId, Mirror as PrimaryMirror, Site},
     relational::Layout,
-    writable::WritableAgent,
+    writable::WritableStore,
     NotificationSender,
 };
 use crate::{
@@ -254,7 +254,7 @@ pub struct SubgraphStoreInner {
     sites: TimedCache<DeploymentHash, Site>,
     placer: Arc<dyn DeploymentPlacer + Send + Sync + 'static>,
     sender: Arc<NotificationSender>,
-    writables: Mutex<HashMap<DeploymentId, Arc<WritableAgent>>>,
+    writables: Mutex<HashMap<DeploymentId, Arc<WritableStore>>>,
 }
 
 impl SubgraphStoreInner {
@@ -1124,7 +1124,7 @@ impl SubgraphStoreTrait for SubgraphStore {
         .await
         .unwrap()?; // Propagate panics, there shouldn't be any.
 
-        let writable = Arc::new(WritableAgent::new(self.as_ref().clone(), logger, site).await?);
+        let writable = Arc::new(WritableStore::new(self.as_ref().clone(), logger, site).await?);
         self.writables
             .lock()
             .unwrap()
